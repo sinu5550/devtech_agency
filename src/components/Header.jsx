ebccea38459fb9ca4631eb617c7802b338849b05
sinu5550 <intisar.siyan5550@gmail.com>
2025-11-11 22:24:33 +0600

    Animatoin added in hero section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,6 +18,40 @@ const Header = () => {
                 y: -50, 
                 opacity: 0,
             });
+
+            const logoEl = logoRef.current;
+    const text = logoEl.textContent;
+    logoEl.textContent = ""; 
+    const letters = text.split("");
+
+    letters.forEach((letter) => {
+      const span = document.createElement("span");
+      span.textContent = letter;
+      span.style.display = "inline-block";
+      span.style.opacity = 0;
+      logoEl.appendChild(span);
+    });
+
+    const typingTl = gsap.timeline();
+    typingTl.to(logoEl.querySelectorAll("span"), {
+      opacity: 1,
+      y: 0,
+      duration: 0.3,
+      stagger: 0.2, 
+      ease: "power1.inOut",
+      onStart: function () {
+        gsap.set(logoEl.querySelectorAll("span"), { y: 20 });
+      },
+    });
+
+   
+    // typingTl.to(logoEl, {
+    //   y: -30,
+    //   duration: 1.5,
+    //   repeat: -1,
+    //   yoyo: true,
+    //   ease: "power1.inOut",
+    // });
             
             const tl = gsap.timeline({
                 scrollTrigger: {
@@ -39,6 +73,14 @@ const Header = () => {
                 duration: 1,
             }, 0); 
             
+            gsap.to(logoRef.current, {
+                y: -30,
+                duration: 1.5,
+                repeat: -1,
+                yoyo: true,
+                ease: "power1.inOut",
+              });
+
             gsap.to(logoRef.current, {
                 scrollTrigger: {
                     trigger: logoRef.current,
@@ -104,7 +146,7 @@ const Header = () => {
                 <div className='h-[90px]'></div> 
                 <h1 
                     ref={logoRef} 
-                    className='font-bold tracking-wider text-[200px] hidden md:flex justify-center mt-12 mb-20 relative z-40'
+                    className='logo-main font-bold tracking-wider text-[200px] hidden md:flex justify-center mt-12 mb-12 relative z-40'
                 >
                     DevTech.
                 </h1>
