b5b69bb9d97b7c134b4c71cd64b6b4e0f01cc9db
Intisar Ahmed Siyan <intisar.siyan5550@gmail.com>
2025-11-15 02:03:17 +0600

    Footer added

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar'
-import { Outlet } from 'react-router'
-import ScrollToTop from '../Scroll/ScrollToTop'
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import { Outlet } from "react-router";
+import ScrollToTop from "../Scroll/ScrollToTop";
+import Footer from "../Footer/Footer";
 
 const Root = () => {
   return (
     <div>
-      <ScrollToTop/>
-        <Navbar></Navbar>
-        <Outlet></Outlet>
+      <ScrollToTop />
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
     </div>
-  )
-}
+  );
+};
 
-export default Root
+export default Root;
